4f4ac4072e00888037ae421f907fc0ae212d2907
Erik Mason <erik@pikhyle.net>
2023-09-22 19:34:13 -0400

    Updates a "UTF-8" string in the new PrintWriter initialization to a StandardCharsets constant.

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -2,6 +2,7 @@ package com.craftinginterpreters.tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,7 +25,7 @@ public class GenerateAst {
             String outputDir, String baseName, List<String> types
     ) throws IOException {
         String path = outputDir + "/" + baseName + ".java";
-        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
         writer.println("package com.craftinginterpreters.lox;");
         writer.println();
