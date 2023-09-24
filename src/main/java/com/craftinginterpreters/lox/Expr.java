8d2baf41bc96dbc28b872d9e7b9193734ba3d258
Erik Mason <info@techlabllc.net>
2023-09-24 02:11:12 -0400

    Native Functions. Introduces native functions clock() and most notably, println(). println() takes the place of the print statement, a la python 2 -> 3.

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import java.util.List;
+
 abstract class Expr {
     abstract <R> R accept(Visitor<R> visitor);
 
@@ -7,6 +9,8 @@ abstract class Expr {
         R visitAssignExpr(Assign expr);
 
         R visitBinaryExpr(Binary expr);
+
+        R visitCallExpr(Call expr);
 
         R visitGroupingExpr(Grouping expr);
 
@@ -38,6 +42,7 @@ abstract class Expr {
         final Expr left;
         final Token operator;
         final Expr right;
+
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
@@ -47,6 +52,23 @@ abstract class Expr {
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBinaryExpr(this);
+        }
+    }
+
+    static class Call extends Expr {
+        final Expr callee;
+        final Token paren;
+        final List<Expr> arguments;
+
+        Call(Expr callee, Token paren, List<Expr> arguments) {
+            this.callee = callee;
+            this.paren = paren;
+            this.arguments = arguments;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitCallExpr(this);
         }
     }
 
@@ -80,6 +102,7 @@ abstract class Expr {
         final Expr left;
         final Token operator;
         final Expr right;
+
         Logical(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
